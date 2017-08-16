96c061528bbe76d62d24ad0a80571548ee6451a1
fmmricardo <ricardofilipe5@sapo.pt>
2017-08-16 07:14:15 +0200

    new main file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 //Variables
 //Pagination
- $pagination = $('.pagination');
- $studentList = $('.student-list li');
+ let pagination = $('.pagination');
+ let studentList = $('.student-list li');
+ let currentList = studentList;
 
 //searchbar
 
@@ -9,29 +10,35 @@
  let studentEmail =$('.email');
  let studentName =$('h3');
 
-// $searchButton= $('.searchButton');
-
+// add pagination div dinamicaly
+$('.page').append('<div class="pagination"></div>');
 
 //Show the page number
-const showPage=(page)=> {
-  // first hide all students on the page
-  $studentList.hide();
-  //slice() method to show only 10 pages
-  $studentList.slice((page * 10) - 10, page * 10).show();
+const showPage= (pageNumber, listName) =>{
+    // first hide all students on the page
+    studentList.hide();
+    let currentListLength = listName.length;
+    // Then loop through all students in our student list argument
+    for (let i = 0; i < currentListLength; i++) {
+   if (i < pageNumber * 10 && i + 1 > (pageNumber - 1) * 10) {
+     $(listName[i]).show();
+   }
+ }
+ currentList= listName;
 };
 
 const appendPageLinks= ()=>{
+    let currentListLength = currentList.length;
   // determine how many pages for this student list
-  const totalPages = Math.ceil($studentList.length / 10);
+  let totalPages = Math.ceil(currentListLength / 10);
   // create a page link section
-  $pagination.append('<ul><ul>');
+  pagination.append('<ul><ul>');
   // “for” every page
   for (let i = 1; i <= totalPages; i++) {
     // add a page link to the page link section
-    $pagination.children('ul').append('<li><a href="#">' + i + '</a></li>');
-
+    pagination.children('ul').append('<li><a href="#">' + i + '</a></li>');
   }
-  $pagination.find('ul li:first a').addClass('active');
+  pagination.find('ul li:first a').addClass('active');
 };
 // append our new page link section to the site
 const loadLink = (link) => {
@@ -42,87 +49,82 @@ const loadLink = (link) => {
   //call the function showPage and pass the link.text
   showPage(link.text());
 };
-
-//initialize pagination
-showPage(1);
-appendPageLinks();
-
+showPage(1, studentList);
 // add loadLink function to each link
 $('.pagination a').on('click', (event) => {
   loadLink($(event.target));
 });
 
-//student search HTML to add dynamically
-$(pageHeader).append('<input id="inputBoxText" placeholder="Search for students..."><button id="searchButton">Search</button>');
+//initialize pagination
 
-function searchList() {
-    //create an array to assinge the founded values (email,name )
-    let foundedResults = [];
-    // Obtain the value of the search input
-    let searchInputBox = $('#inputBoxText').val()//.toLowerCase();
-    // clear the links
+appendPageLinks();
 
 
-    // Loop over the student list, and for each student…
-    for(let i=0; i<$studentList.length; i++){
-      // ...obtain the student’s name…
-      // ...and the student’s email…
-      if(name[i].textContent.search(searchInputBox)!= -1 || emails[i].textContent.search(searchInputBox)!= -1){
-        // push the founded fields to the array
-        foundedResults.push(studentList[i]);
-      }
-}
 
-// ...if the search value is found inside either email or name…
-if (foundedResults.length=== 0){
-  //hide studentList
-  $studentList.hide();
-  //remove the previous faile search
-  $('.noResult').remove();
-  // add a div with the message "No result found"
-  $('.page').append('<div class="noResult">No result Found!</div>');
-  // ...add this student to list of “matched” student
-}else if (foundedResults.length < 10) {
-  //remove the previoua search
-  $('.noResult').remove();
-  //hide setudentsList
-  $studentList.hide();
-  //show(page,foundedResults)
-  showPage(1);
-  //pagination
-}else{
-    //remove the previous faile search
-    $('.noResult').remove();
-    //hide studentList
-    $studentList.hide();
-    //show results
-    showPage(1);
-    //add pagionation Link
-    appendPageLinks();
-  }
-//clear the input field
-document.getElementById('searchInputBox').value = '';
-}
+// //student search HTML to add dynamically
+// $(pageHeader).append('<input id="inputBoxText" placeholder="Search for students..."><button id="searchButton">Search</button>');
 //
-
-//create a EventListner for the button
-
-$('#searchButton').on('click',()=>{
-searchList();
-});
-
-
-//create a EventListner for the Enter key
-
-$('#searchButton').on('keyup',()=>{
-  if (13 == e.keyCode) {
-     searchList();
-  }
-});
-
-
-
-    // If there’s no “matched” students…
-           // ...display a “no student’s found” message
-    // If over ten students were found…
-           //
+// function searchList() {
+//     //create an array to assinge the founded values (email,name )
+//     let foundedResults = [];
+//     // Obtain the value of the search input
+//     let searchInputBox = $('#inputBoxText').val()//.toLowerCase();
+//     // clear the links
+//
+//
+//     // Loop over the student list, and for each student…
+//     for(let i=0; i<$studentList.length; i++){
+//       // ...obtain the student’s name…
+//       // ...and the student’s email…
+//       if(studentName[i].textContent.search(searchInputBox)!= -1 || studentEmail[i].textContent.search(searchInputBox)!= -1){
+//         // push the founded fields to the array
+//         foundedResults.push(studentList[i]);
+//       }
+// }
+//
+// // ...if the search value is found inside either email or name…
+// if (foundedResults.length=== 0){
+//   //hide studentList
+//   $studentList.hide();
+//   //remove the previous faile search
+//   $('.noResult').remove();
+//   // add a div with the message "No result found"
+//   $('.page').append('<div class="noResult">No result Found!</div>');
+//   // ...add this student to list of “matched” student
+// }else if (foundedResults.length < 10) {
+//   //remove the previoua search
+//   $('.noResult').remove();
+//   //hide setudentsList
+//   $studentList.hide();
+//   //show(page,foundedResults)
+//   showPage(1);
+//   //pagination
+// }else{
+//     //remove the previous faile search
+//     $('.noResult').remove();
+//     //hide studentList
+//     $studentList.hide();
+//     //show results
+//     showPage(1);
+//     //add pagionation Link
+//     appendPageLinks();
+//   }
+// //clear the input field
+// document.getElementById('searchInputBox').value = '';
+// }
+// //
+//
+// //create a EventListner for the button
+//
+// $('#searchButton').on('click',()=>{
+// searchList();
+// });
+//
+//
+// //create a EventListner for the Enter key
+//
+// $('#searchButton').on('keyup',()=>{
+//   if (13 == e.keyCode) {
+//      searchList();
+//   }
+// });
